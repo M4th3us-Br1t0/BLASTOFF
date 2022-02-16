25a8ab633fcde3a50757d8a32d3e026163c0e27c
M4th3us-Br1t0 <97305450+M4th3us-Br1t0@users.noreply.github.com>
2022-02-16 16:19:10 -0300

    Add files via upload

---
+++ Exercicio01.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApplicationVSCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i,j,k,x,y,media;
+            Console.WriteLine("Digite a primeira nota: ");
+            int.TryParse(Console.ReadLine(), out i);
+            Console.WriteLine("Digite a segunda nota: ");
+            int.TryParse(Console.ReadLine(), out j);
+            Console.WriteLine("Digite a terceira nota: ");
+            int.TryParse(Console.ReadLine(), out k);
+            Console.WriteLine("Digite a quarta nota: ");
+            int.TryParse(Console.ReadLine(), out x);
+            Console.WriteLine("Digite a quinta nota: ");
+            int.TryParse(Console.ReadLine(), out y);
+
+            media=(i+j+k+x+y)/5;
+
+            Console.WriteLine("Média: "+ media);
+        }
+    }
+}
